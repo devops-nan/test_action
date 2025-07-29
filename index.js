b47ac1da75672f3e3e70a16c214d4e6ce5378ffa
nancygv <110550654+nancygv@users.noreply.github.com>
2025-07-28 22:31:42 -0600

    fix: update greeting message to use Spanish

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const core = require('@actions/core');
 try {
   const name = core.getInput('name');
-  console.log(`Hello ${name}!`);
+  console.log(`Hola ${name}`);
   run();
 } catch (error) {
   core.setFailed(error.message);
